605a1acb0585004631437200aa369cf80f29538b
Jason Mulvenna <jason.mulvenna@gmail.com>
2018-01-28 15:56:58 +1000

    Documentation updates

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject vmaths "0.1.0"
+(defproject vmaths "0.1.1"
   :description "Some common maths functions for vectors."
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
